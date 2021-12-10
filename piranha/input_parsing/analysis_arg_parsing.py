a93d4f5e54ce6c649fa11e7159ccb8c9a9314a26
aineniamh <aine.otoole@ed.ac.uk>
2021-12-10 15:00:39 +0000

    rm ref seq parsing and move to input

--- piranha/input_parsing/analysis_arg_parsing.py
+++ piranha/input_parsing/analysis_arg_parsing.py
@@ -5,7 +5,7 @@ import yaml
 from piranha.utils.log_colours import green,cyan
 from piranha.utils.config import *
 
-from pirahna.utils import misc
+from piranha.utils import misc
 import piranha.utils.custom_logger as custom_logger
 import piranha.utils.log_handler_handle as lh
 
@@ -28,5 +28,3 @@ def analysis_group_parsing(min_read_length,max_read_length,min_read_depth,min_re
     for key in [KEY_MIN_READ_LENGTH,KEY_MAX_READ_LENGTH,KEY_MIN_READS,KEY_MIN_PCENT]:
         check_if_int(key,config)
 
-    misc.add_file_to_config(KEY_REFERENCE_SEQUENCES,reference_sequences,config)
-    misc.check_path_exists(config[KEY_REFERENCE_SEQUENCES])
