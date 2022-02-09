6192a92a82072a760849f9a6eb341d9c373a21b3
aineniamh <aine.otoole@ed.ac.uk>
2022-02-09 23:00:03 +0000

    variant sites and tooltip info

--- piranha/analysis/get_haplotypes.py
+++ piranha/analysis/get_haplotypes.py
@@ -25,14 +25,34 @@ def get_variation_pcent(ref,fasta):
             if pos[0] != pos[1]:
                 variant_sites[index]+=1
             index +=1
+
+    variant_info = {}
+    for site in variant_sites:
+        variant_info[site] = collections.Counter()
+
+    for record in SeqIO.parse(fasta,"fasta"):
+        for site in variant_sites:
+            variant = str(record.seq)[site]
+            variant_info[site][variant]+=1
+
     x = []
     y = []
+    info = []
+    variation_info = []
     for site in variant_sites:
+        
         pcent_variants = round(100*(variant_sites[site]/c), 1)
-        x.append(site+1)
-        y.append(pcent_variants)
-    var_dict = {"x":x,"y":y}
-    return var_dict
+        x = site+1
+        y = pcent_variants
+
+        var_counts = variant_info[site]
+        site_data = {"Position":x,"Percentage":y}
+        for i in var_counts:
+            site_data[f"{i} reads"] = var_counts[i]
+        
+        variation_info.append(site_data)
+
+    return variation_info
 
 def parse_vcf(fasta,vcf,min_reads,min_pcent,taxon,haplotypes_out):
     variant_sites = []
@@ -46,34 +66,37 @@ def parse_vcf(fasta,vcf,min_reads,min_pcent,taxon,haplotypes_out):
                 
     site_str = ';'.join([str(i) for i in variant_sites])
     
-    
+    print(site_str)
     haplo_counter = collections.Counter()
     haplo_records = collections.defaultdict(list)
     read_count = 0
     for record in SeqIO.parse(fasta,"fasta"):
         read_count +=1
-        read_haplotype = "".join([record.seq[i] for i in variant_sites])
+        read_haplotype = ""
+        for i in variant_sites:
+            read_haplotype+=f"{i}{record.seq[i-1]}"
         haplo_counter[read_haplotype] +=1
-
+    print(haplo_counter)
     for record in SeqIO.parse(fasta,"fasta"):
-        read_haplotype = "".join([record.seq[i] for i in variant_sites])
+        read_haplotype = ""
+        for i in variant_sites:
+            read_haplotype+=f"{i}{record.seq[i-1]}"
         if 100*(haplo_counter[read_haplotype]/read_count) > min_pcent:
             haplo_records[read_haplotype].append(record.id)
-    
+
     read_haplotypes = {}
     print(green("Haplotypes identified\n--------\n--------"))
     print("Sites: ", site_str)
     with open(haplotypes_out,"w") as fw:
-        fw.write(f"taxon,sites,haplotype,num_reads,make_cns,read_ids\n")
+        fw.write(f"taxon,sites,haplotype,num_reads,make_cns\n")
         for i in haplo_records:
             num_reads = len(haplo_records[i])
-            read_str = ';'.join(haplo_records[i])
             if num_reads > min_reads:
                 print(green(f"{i}: ") + f"{len(haplo_records[i])}")
                 read_haplotypes[i] = haplo_records[i]
-                fw.write(f"{taxon},{site_str},{i},{num_reads},True,{read_str}\n")
+                fw.write(f"{taxon},{site_str},{i},{num_reads},True\n")
             else:
-                fw.write(f"{taxon},{site_str},{i},{num_reads},False,{read_str}\n")
+                fw.write(f"{taxon},{site_str},{i},{num_reads},False\n")
     return read_haplotypes
 
 def write_haplotype_ref(ref,taxon,read_haplotypes,outdir):
@@ -98,7 +121,6 @@ def write_haplotype_fastq(reads,taxon,read_haplotypes,outdir):
 def get_haplotypes(fasta,vcf,reads,ref,out_haplotypes,outdir,taxon,min_reads,min_pcent):
     if not os.path.exists(outdir):
         os.mkdir(outdir)
-
 
     haps = parse_vcf(fasta,vcf,min_reads,min_pcent,taxon,out_haplotypes)
 
