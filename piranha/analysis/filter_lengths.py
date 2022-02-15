9b9a97f7544534d7b59c6d15cdcc58c5fdb09ebc
aineniamh <aine.otoole@ed.ac.uk>
2022-02-15 14:55:22 +0000

    don't need len dist

--- piranha/analysis/filter_lengths.py
+++ piranha/analysis/filter_lengths.py
@@ -4,20 +4,14 @@ import collections
 from piranha.utils.config import *
 
 
-def filter_reads_by_length(reads_in,reads_out,lengths_out,config):
+def filter_reads_by_length(reads_in,reads_out,config):
     fastq_records = []
-    length_list = []
 
     with open(reads_out,"w") as fw:
         for record in SeqIO.parse(reads_in,"fastq"):
             length = len(record)
-            length_list.append(length)
             if length > int(config[KEY_MIN_READ_LENGTH]) and length < int(config[KEY_MAX_READ_LENGTH]):
                 fastq_records.append(record)
 
         SeqIO.write(fastq_records,fw, "fastq")
-    with open(lengths_out,"w") as fw:
-        # this is in case we want to make a read length dist in the report
-        for i in length_list:
-            fw.write(f"{i}\n")
-        fw.write("\n")
+    
