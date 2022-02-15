4d54b4a5376fa0251e93cdb7ecd26c9f79f37fd2
aineniamh <aine.otoole@ed.ac.uk>
2022-02-15 14:51:53 +0000

    header fields configured rather than typed now

--- piranha/utils/config.py
+++ piranha/utils/config.py
@@ -28,7 +28,12 @@ KEY_MIN_PCENT = "min_read_pcent"
 KEY_REFERENCE_SEQUENCES = "reference_sequences"
 
 KEY_BARCODE = "barcode"
-KEY_TAXA_PRESENT = "taxa_present"
+KEY_SAMPLE = "sample"
+KEY_REFERENCE = "reference"
+KEY_NUM_READS = "num_reads"
+KEY_PERCENT = "percent_of_sample"
+KEY_REFERENCE_GROUP = "reference_group"
+KEY_VIRUS_PRESENT = "virus_present"
 KEY_TAXA_OUTDIR = "taxa_outdir"
 KEY_HITS = "hits"
 
@@ -49,6 +54,24 @@ RESOURCE_KEY="KEY"
 
 KEY_SUMMARY_HEADERS = "report_summary_headers"
 
+# default values for config dict
+VALUE_OUTPUT_PREFIX = "analysis"
+VALUE_SUMMARY_HEADERS = ["taxon","sites","haplotype","num_reads","make_cns"]
+VALUE_REFERENCES_FOR_CNS = ["Sabin1-related","Sabin2-related","Sabin3-related","WPV1"]
+VALUE_ANALYSIS_MODE = "stool"
+VALUE_MIN_READ_LENGTH = 1000
+VALUE_MAX_READ_LENGTH = 1300
+VALUE_MIN_READS = 50
+VALUE_MIN_PCENT = 5
+
+VALUE_COLOUR_MAP=["#e68781","#476970","#f5eece"]
+VALUE_COLOUR_THEME="#e68781"
+
+#file headers
+
+SAMPLE_HIT_HEADER_FIELDS = ["barcode","reference","reference_group","num_reads","percent_of_sample"]
+SAMPLE_SUMMARY_HEADER_FIELDS = ["sample","barcode","Sabin1-related","Sabin2-related","Sabin3-related",
+                                "WPV1","WPV2","WPV3","NonPolioEV","unmapped","virus_present"]
 
 # DEPENDENCIES AND RESOURCES TO CHECK
 valid_analysis_modes = ["stool","environmental"]
