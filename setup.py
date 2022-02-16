dd8db1ea90ed93742e4486ca3a85ee280e79bae3
aineniamh <aine.otoole@ed.ac.uk>
2022-02-16 16:50:31 +0000

    new pipelines to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name='piranha',
       scripts=[
             "piranha/scripts/piranha_preprocessing.smk",
             "piranha/scripts/piranha_stool.smk",
+            "piranha/scripts/consensus.smk",
             "piranha/scripts/piranha_environmental.smk",
             "piranha/scripts/haplotype_consensus.smk",
             "piranha/scripts/snipit.smk",
