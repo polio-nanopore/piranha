39a73ee3efd6ab06aeda378f53d6cde8bd0770be
aineniamh <aine.otoole@ed.ac.uk>
2022-02-18 15:08:00 +0000

    writing out fa file for each barcode separately

--- piranha/analysis/stool_functions.py
+++ piranha/analysis/stool_functions.py
@@ -6,7 +6,10 @@ import csv
 
 from piranha.utils.config import *
 
-def gather_fasta_files(summary_info, barcodes_csv, input_cns_list, output_file):
+def gather_fasta_files(summary_info, barcodes_csv, input_cns_list, output_file,publish_dir):
+    if not os.path.exists(publish_dir):
+        os.mkdir(publish_dir)
+    
     analysis_info = collections.defaultdict(list)
     with open(summary_info, "r") as f:
         reader = csv.DictReader(f)
@@ -14,10 +17,17 @@ def gather_fasta_files(summary_info, barcodes_csv, input_cns_list, output_file):
             analysis_info[row[KEY_BARCODE]].append(row)
 
     input_metadata = {}
+    handle_dict = {}
     with open(barcodes_csv,"r") as f:
         reader = csv.DictReader(f)
         for row in reader:
-            input_metadata[row[KEY_BARCODE]] = row
+            barcode = row[KEY_BARCODE]
+            input_metadata[barcode] = row
+            
+            if barcode not in handle_dict:
+                if not os.path.exists(os.path.join(publish_dir, f"{barcode}")):
+                    os.mkdir(os.path.join(publish_dir, f"{barcode}"))
+                handle_dict[barcode] = open(os.path.join(publish_dir, f"{barcode}",f"{barcode}.consensus.fasta"),"w")
 
     with open(output_file,"w") as fw:
         for cns_file in input_cns_list:
@@ -30,9 +40,13 @@ def gather_fasta_files(summary_info, barcodes_csv, input_cns_list, output_file):
                     if row[KEY_REFERENCE] == ref:
                         info = row
                 metadata = input_metadata[barcode]
-
+                
                 record_id = f"{metadata[KEY_SAMPLE]}|{barcode}|{info[KEY_REFERENCE_GROUP]}|{ref}|{var_count}|{var_string}|{metadata[KEY_DATE]}"
                 fw.write(f">{record_id}\n{record.seq}\n")
+                handle_dict[barcode].write(f">{record_id}\n{record.seq}\n")
+    
+    for handle in handle_dict:
+        handle_dict[handle].close()
 
 def get_sample(barcodes_csv,barcode):
     sample = ""
