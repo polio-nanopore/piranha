4e4c7e70f5595152be3b3bcfb9afe482ea0f16fd
aineniamh <aine.otoole@ed.ac.uk>
2023-09-09 09:28:05 +0100

    write out annotations file for each tree and for all background annotations

--- piranha/analysis/phylo_functions.py
+++ piranha/analysis/phylo_functions.py
@@ -9,13 +9,22 @@ from piranha.utils.log_colours import green,cyan,red
 
 
 def get_seqs_and_clusters(sample_seqs,supplementary_sequences,reference_sequences,outgroup_sequences,phylo_outdir,config):
+    
+    seq_metadata = collections.defaultdict(dict)
     seq_clusters = collections.defaultdict(list)
     for record in SeqIO.parse(sample_seqs,"fasta"):
         for ref_group in config[KEY_REFERENCES_FOR_CNS]:
             if ref_group in record.id:
                 new_record = record
-                new_record.description = "|".join(new_record.id.split("|")[:2])
-                seq_clusters[ref_group].append(record)
+                new_header = "|".join(new_record.id.split("|")[:2])
+                new_record.description = new_header
+                new_record.id = new_header
+                seq_clusters[ref_group].append(new_record)
+
+                seq_metadata[new_record.id]["sample"] = new_record.id
+                seq_metadata[new_record.id]["query_boolean"] = "True"
+                seq_metadata[new_record.id]["reference_group"] = ref_group
+
 
     print(green("Reference groups for phylo pipeline:"))
     for i in seq_clusters:
@@ -26,24 +35,51 @@ def get_seqs_and_clusters(sample_seqs,supplementary_sequences,reference_sequence
             for ref_group in seq_clusters:
                 if ref_group in record.description:
                     seq_clusters[ref_group].append(record)
+
+                    seq_metadata[record.id]["sample"] = record.id
+                    seq_metadata[record.id]["query_boolean"] = "False"
+                    seq_metadata[record.id]["reference_group"] = ref_group
     
     for record in SeqIO.parse(reference_sequences, "fasta"):
         for ref_group in seq_clusters:
             if ref_group in record.description:
                 seq_clusters[ref_group].append(record)
+
+                seq_metadata[record.id]["sample"] = record.id
+                seq_metadata[record.id]["query_boolean"] = "False"
+                seq_metadata[record.id]["reference_group"] = ref_group
     
     for record in SeqIO.parse(outgroup_sequences, "fasta"):
         for ref_group in seq_clusters:
             if ref_group in record.description:
                 new_record = record
+                new_record.id = "outgroup"
                 new_record.description = "outgroup"
                 seq_clusters[ref_group].append(new_record)
 
-    for i in seq_clusters:
-        print(i, len(seq_clusters[i]))
+    with open(os.path.join(phylo_outdir, f"annotations.csv"), "w") as fw0:
+        header = ["sample","query_boolean","reference_group"]
+        writer0 = csv.DictWriter(fw0,fieldnames=header, lineterminator='\n')
+        writer0.writeheader()
 
-        with open(os.path.join(phylo_outdir, f"{i}.fasta"),"w") as fw:
-            SeqIO.write(seq_clusters[i], fw, "fasta")
+        for i in seq_clusters:
+            print(i, len(seq_clusters[i]))
+
+            with open(os.path.join(phylo_outdir, f"{i}.annotations.csv"), "w") as fw:
+                
+                writer = csv.DictWriter(fw,fieldnames=header, lineterminator='\n')
+                writer.writeheader()
+                for record in seq_metadata:
+                    if seq_metadata[record]["reference_group"] == i:
+                        row = seq_metadata[record]
+                        for col in header:
+                            new_data = row[col].replace("'","").replace(";","").replace("(","").replace(")","")
+                            row[col] = new_data
+                        writer.writerow(row)
+                        writer0.writerow(row)
+
+            with open(os.path.join(phylo_outdir, f"{i}.fasta"),"w") as fw:
+                SeqIO.write(seq_clusters[i], fw, "fasta")
 
     return list(seq_clusters.keys())
 
