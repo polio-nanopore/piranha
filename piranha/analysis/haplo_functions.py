4edb623bce33dd7112c26ff6190c2f44fe43e5ec
aineniamh <aine.otoole@ed.ac.uk>
2023-11-10 13:42:54 +0000

    changing style from js camelcase to python snake case for consistency within piranha

--- piranha/analysis/haplo_functions.py
+++ piranha/analysis/haplo_functions.py
@@ -27,64 +27,64 @@ def parse_partition_file(partition_file):
 
 
 
-def collapseClose(floppFile,distanceCutoff,vcfFile):
-    haploPos = 0
+def collapse_close(flopp_file,distance_cutoff,vcf_file):
+    haplo_pos = 0
     collapsed = []
     output = []
-    finalHaplos = []
-    vcf = parseVCF(vcfFile)
-    parsedFlopp = parseFloppReads(floppFile)
-    for h in parsedFlopp:
-        collapseIntoCurrent = [haploPos]
-        if haploPos in collapsed:
+    final_haplos = []
+    vcf = parse_VCF(vcf_file)
+    parsed_flopp = parse_flopp_reads(flopp_file)
+    for h in parsed_flopp:
+        collapse_into_current = [haplo_pos]
+        if haplo_pos in collapsed:
             #don't consider haplotypes already merged into another
-            haploPos += 1
+            haplo_pos += 1
             continue
-        newH = {}
-        foundOthers = False
+        new_h = {}
+        found_others = False
         
         #for each haplo, check if any others are identical/within distance cutoff, collapse in and combine reads if so, remove collapsed haplo from further consideration
-        haploPos2 = haploPos +1
-        for h2 in parsedFlopp[haploPos2:]:
-            distance = getHaploDist(h,h2,vcf)
-            if distance <= distanceCutoff and newH == {}:
+        haplo_pos2 = haplo_pos +1
+        for h2 in parsed_flopp[haplo_pos2:]:
+            distance = get_haplo_dist(h,h2,vcf)
+            if distance <= distance_cutoff and new_h == {}:
                 #collapse values and create new haplo 
                 for pos in h:
                     #combine
-                    newH[pos] = {}
-                    newH[pos]['total assigned'] = h[pos]['total assigned'] + h2[pos]['total assigned']
-                    newH[pos]['base support counts'] = sumBaseSupport(h[pos]['base support counts'],h2[pos]['base support counts'])
+                    new_h[pos] = {}
+                    new_h[pos]['total assigned'] = h[pos]['total assigned'] + h2[pos]['total assigned']
+                    new_h[pos]['base support counts'] = sum_base_support(h[pos]['base support counts'],h2[pos]['base support counts'])
                     #check if allele assignment is different now that reads are collapsed
-                    newH[pos]['assigned allele'] = max(newH[pos]['base support counts'],key=lambda x:newH[pos]['base support counts'][x])
+                    new_h[pos]['assigned allele'] = max(new_h[pos]['base support counts'],key=lambda x:new_h[pos]['base support counts'][x])
                     
             
-                collapsed.append(haploPos2)
-                collapseIntoCurrent.append(haploPos2)
-                foundOthers = True
+                collapsed.append(haplo_pos2)
+                collapse_into_current.append(haplo_pos2)
+                found_others = True
 
-            elif distance <= distanceCutoff:
+            elif distance <= distance_cutoff:
                 for pos in h:
-                    newH[pos]['total assigned'] = newH[pos]['total assigned'] + h2[pos]['total assigned']
-                    newH[pos]['base support counts'] = sumBaseSupport(newH[pos]['base support counts'],h2[pos]['base support counts'])
+                    new_h[pos]['total assigned'] = new_h[pos]['total assigned'] + h2[pos]['total assigned']
+                    new_h[pos]['base support counts'] = sum_base_support(new_h[pos]['base support counts'],h2[pos]['base support counts'])
                     #check if allele assignment is different now that reads are collapsed
-                    newH[pos]['assigned allele'] = max(newH[pos]['base support counts'],key=lambda x:newH[pos]['base support counts'][x])
+                    new_h[pos]['assigned allele'] = max(new_h[pos]['base support counts'],key=lambda x:new_h[pos]['base support counts'][x])
                         
-                collapsed.append(haploPos2)
-            haploPos2 += 1
+                collapsed.append(haplo_pos2)
+            haplo_pos2 += 1
 
-        if not foundOthers:
-            newH = h
+        if not found_others:
+            new_h = h
         
         #need these merged versions of parsed flopp output to generate even-ness, maybe make one do-all function
-        finalHaplos.append(newH)
-        haploPos += 1
-        output.append(collapseIntoCurrent)
+        final_haplos.append(new_h)
+        haplo_pos += 1
+        output.append(collapse_into_current)
         
     return output
 
-def parseVCF(vcfFile):
-    vcfInfo = {}
-    with open(vcfFile) as file:
+def parse_VCF(vcf_file):
+    vcf_info = {}
+    with open(vcf_file) as file:
         for line in file:
             if line.startswith('#'):
                 continue
@@ -92,33 +92,33 @@ def parseVCF(vcfFile):
                 line = line.strip('\n').split('\t')
                 pos, ref, alt, info = line[1], line[3], line[4], line[7]
                 pos = int(pos)
-                infoDict = dict([x.split('=') for x in info.split(';')])
-                vcfInfo[pos] = {}
-                vcfInfo[pos]['0'] = ref
+                info_dict = dict([x.split('=') for x in info.split(';')])
+                vcf_info[pos] = {}
+                vcf_info[pos]['0'] = ref
                 if ',' in alt:
                     #multiallelic SNP
-                    vcfInfo[pos]['1'], vcfInfo[pos]['2'] = alt.split(',')
+                    vcf_info[pos]['1'], vcf_info[pos]['2'] = alt.split(',')
                 else:
-                    vcfInfo[pos]['1'] = alt 
-                vcfInfo[pos]['readCounts'] = {}
-                refReads = infoDict['RO']
-                altReads = infoDict['AO']
+                    vcf_info[pos]['1'] = alt 
+                vcf_info[pos]['read_counts'] = {}
+                ref_reads = info_dict['RO']
+                alt_reads = info_dict['AO']
                 #float because freebayes can have 'fractional' observations
-                vcfInfo[pos]['readCounts']['0'] = float(refReads)
-                if ',' in altReads:
-                    counts = [float(x) for x in altReads.split(',')]
-                    vcfInfo[pos]['readCounts']['1'] = counts[0]
-                    vcfInfo[pos]['readCounts']['2'] = counts[1]
+                vcf_info[pos]['read_counts']['0'] = float(ref_reads)
+                if ',' in alt_reads:
+                    counts = [float(x) for x in alt_reads.split(',')]
+                    vcf_info[pos]['read_counts']['1'] = counts[0]
+                    vcf_info[pos]['read_counts']['2'] = counts[1]
                 else:
-                    vcfInfo[pos]['readCounts']['1'] = float(altReads)
-                vcfInfo[pos]['frequencies'] = {}
-                for allele in vcfInfo[pos]['readCounts']:
-                    vcfInfo[pos]['frequencies'][allele] = vcfInfo[pos]['readCounts'][allele]/sum(vcfInfo[pos]['readCounts'].values())
+                    vcf_info[pos]['read_counts']['1'] = float(alt_reads)
+                vcf_info[pos]['frequencies'] = {}
+                for allele in vcf_info[pos]['read_counts']:
+                    vcf_info[pos]['frequencies'][allele] = vcf_info[pos]['read_counts'][allele]/sum(vcf_info[pos]['read_counts'].values())
                 #frequencies list allele freqs in 0,1,2 order
 
-    return vcfInfo
+    return vcf_info
 
-def parseFloppReads(floppPath):
+def parse_flopp_reads(floppPath):
     #returns an array of haplotypes; each haplo is a dict with genomic positions as keys, each pos has a dict with allele, read support
     #might need to make some changes for more than 2 alt alleles
     #TODO change to recording values for all bases
@@ -132,16 +132,16 @@ def parseFloppReads(floppPath):
                 pos = line[0].split(':')[1]
                 pos=int(pos)
                 k = int((len(line) - 1)/2) #number of haplotypes
-                hapCols = line[1:k+1]
-                supportCols = line[k+1:]
+                hap_cols = line[1:k+1]
+                support_cols = line[k+1:]
                 
                 for i in range(k):
                     haps.append({})
-                    outDict = {}
-                    outDict['assigned allele'] = hapCols[i]
+                    out_dict = {}
+                    out_dict['assigned allele'] = hap_cols[i]
                     #something has to change here, want reads for all possible alleles
-                    outDict['total assigned'], outDict['base support counts'] = parseSupportCol(supportCols[i])
-                    haps[i][pos] = outDict
+                    out_dict['total assigned'], out_dict['base support counts'] = parse_support_col(support_cols[i])
+                    haps[i][pos] = out_dict
 
             elif line == '*****\n':
                 #EOF
@@ -151,75 +151,75 @@ def parseFloppReads(floppPath):
                 line = line.strip('\n').split('\t')
                 pos = line[0].split(':')[1]
                 pos=int(pos)
-                hapCols = line[1:k+1]
-                supportCols = line[k+1:]
+                hap_cols = line[1:k+1]
+                support_cols = line[k+1:]
 
                 for i in range(k):
-                    outDict = {}
-                    outDict['assigned allele'] = hapCols[i]
-                    outDict['total assigned'], outDict['base support counts'] = parseSupportCol(supportCols[i])
-                    haps[i][pos] = outDict
+                    out_dict = {}
+                    out_dict['assigned allele'] = hap_cols[i]
+                    out_dict['total assigned'], out_dict['base support counts'] = parse_support_col(support_cols[i])
+                    haps[i][pos] = out_dict
     return haps
 
-def parseSupportCol(col):
-    baseSplit = col.split('|')
-    totalReads = 0
-    baseSupport = {}
-    for base in baseSplit:
-        baseVal,supportReads = base.split(':')
-        # if baseVal == selBaseVal:
-        #     baseSupport = int(supportReads)
-        baseSupport[baseVal] = int(supportReads)
-        totalReads += int(supportReads)
-    return (totalReads,baseSupport)
+def parse_support_col(col):
+    base_split = col.split('|')
+    total_reads = 0
+    base_support = {}
+    for base in base_split:
+        base_val,support_reads = base.split(':')
+        # if base_val == selbase_val:
+        #     base_support = int(support_reads)
+        base_support[base_val] = int(support_reads)
+        total_reads += int(support_reads)
+    return (total_reads,base_support)
 
-def checkSiteSig(baseSupport,alleleFreqs):
+def check_site_sig(base_support,allele_freqs):
     #check if allele freqs are consistent with random sample from overall reads for this site
     #need to know these are the same order
     #reads might have 100% one allele, need to insert zero in this case, then sort to make sure order correct
-    for key in alleleFreqs:
-        if key not in baseSupport:
-            baseSupport[key] = 0
-    readCounts = [baseSupport[k] for k in sorted(baseSupport)]
-    exp = [alleleFreqs[alleleFreq]*sum(readCounts) for alleleFreq in alleleFreqs]
+    for key in allele_freqs:
+        if key not in base_support:
+            base_support[key] = 0
+    read_counts = [base_support[k] for k in sorted(base_support)]
+    exp = [allele_freqs[allele_freq]*sum(read_counts) for allele_freq in allele_freqs]
     try:
-        pval = chisquare(f_obs=readCounts,f_exp=exp).pvalue
+        pval = chisquare(f_obs=read_counts,f_exp=exp).pvalue
     except Exception as e:
-        print('Observed values for read counts',readCounts)
+        print('Observed values for read counts',read_counts)
         print('Expected values for read counts',exp)
-        print('Allele frequencies',alleleFreqs)
+        print('Allele frequencies',allele_freqs)
         raise ValueError(f'chi squared calculation failed with error, {e}, see above values')
     if pval < 0.01:
         return True
     else:
         return False
 
-def getHaploDist(h1,h2,parsedVCF):
+def get_haplo_dist(h1,h2,parsed_VCF):
     #returns the number of reliable SNPs separating 2 haplotypes
-    sigPos1, sigPos2 = [],[]
+    sig_pos1, sig_pos2 = [],[]
     # for position in each haplo, check significance (i.e. are the base proportions differnt from a random sample of total read set), add to list if is
-    #need a lookup from vcf parsing to get allelefreqs
+    #need a lookup from vcf parsing to get allele_freqs
     for pos in h1:
-        if checkSiteSig(h1[pos]['base support counts'],parsedVCF[pos]['frequencies']) and h1[pos]['assigned allele'] != '0':
+        if check_site_sig(h1[pos]['base support counts'],parsed_VCF[pos]['frequencies']) and h1[pos]['assigned allele'] != '0':
             #need to append both position and allele to list
-            sigPos1.append((pos,h1[pos]['assigned allele']))
+            sig_pos1.append((pos,h1[pos]['assigned allele']))
     for pos in h2:
-        if checkSiteSig(h2[pos]['base support counts'],parsedVCF[pos]['frequencies']) and h2[pos]['assigned allele'] != '0':
-            sigPos2.append((pos,h2[pos]['assigned allele']))
+        if check_site_sig(h2[pos]['base support counts'],parsed_VCF[pos]['frequencies']) and h2[pos]['assigned allele'] != '0':
+            sig_pos2.append((pos,h2[pos]['assigned allele']))
     #how many of the reliable SNPs are only in one
     #set arith?
-    h1Only = set(sigPos1) - set(sigPos2)
-    h2Only = set(sigPos2) - set(sigPos1)
+    h1_only = set(sig_pos1) - set(sig_pos2)
+    h2_only = set(sig_pos2) - set(sig_pos1)
     #this counts the same site with different alt alleles as 2 separate SNPs, maybe needs to change but how often is this going to be an issue
-    return len(h1Only) + len(h2Only)
+    return len(h1_only) + len(h2_only)
 
-def sumBaseSupport(dict1,dict2):
-    outDict = {}
+def sum_base_support(dict1,dict2):
+    out_dict = {}
     for key in dict1:
-        outDict[key] = dict1[key] + dict2[key]
-    return outDict
+        out_dict[key] = dict1[key] + dict2[key]
+    return out_dict
 
-def calcSD(haplotypes):
+def calc_sd(haplotypes):
     haploCount = 1
     evenDict = {}
     for h in haplotypes:
