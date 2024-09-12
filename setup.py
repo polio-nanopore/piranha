eabcedc72274cf57210f6b16521c82e08e4ee99c
aineniamh <aine.otoole@ed.ac.uk>
2024-09-12 10:53:32 +0100

    rm python pin

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='piranha',
             "medaka==2.0.0",
             "numpy",
             "scipy",
-            "torch==2.4.1"
+            "torch==2.2.2"
       ],
       description='piranha: Poliovirus Investigation Resource Automating Nanopore Haplotype Analysis',
       url='https://github.com/polio-nanopore/piranha',
