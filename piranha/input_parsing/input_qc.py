745ab774ca9609880e9895de55ba0b4c85c15d08
aineniamh <aine.otoole@ed.ac.uk>
2025-04-04 15:44:54 +0100

    ignore empty rows in barcodes csv

--- piranha/input_parsing/input_qc.py
+++ piranha/input_parsing/input_qc.py
@@ -61,9 +61,10 @@ def parse_barcodes_csv(barcodes_csv,config):
                 elif special_character in row[KEY_SAMPLE]:
                     sys.stderr.write(cyan(f"Special character `{special_character}` cannot be used in barcode or sample name. Please remove this character from sample `{row[KEY_SAMPLE]}` and restart.\n"))
                     sys.exit(-1)
-
-            barcodes.append(row[KEY_BARCODE])
-            samples.append(row[KEY_SAMPLE])
+                    
+            if row[KEY_BARCODE] and row[KEY_SAMPLE]:
+                barcodes.append(row[KEY_BARCODE])
+                samples.append(row[KEY_SAMPLE])
 
     config[KEY_BARCODES] = barcodes
     config[KEY_SAMPLES] = samples
